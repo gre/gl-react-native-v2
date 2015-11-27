f0708ad1a9074d829c7062501d719b3a63bc6e4b
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2015-11-27 18:24:26 +0100

    Port examples to android

--- Examples/AdvancedEffects/src/index.js
+++ Examples/AdvancedEffects/src/index.js
@@ -2,9 +2,10 @@ const React = require("react-native");
 const {
   StyleSheet,
   View,
-  StatusBarIOS,
 } = React;
 const { width: viewportW, height: viewportH } = require("Dimensions").get("window");
+
+const resolveAssetSource = require("react-native/Libraries/Image/resolveAssetSource");
 
 const Banner = require("./Banner");
 const Intro = require("./Intro");
@@ -18,15 +19,13 @@ class AdvancedEffects extends React.Component {
     this.state = {
       time: 0.02,
       frames: 1,
-      embeddedImage: require("image!Image"),
+      embeddedImage: resolveAssetSource(require("./Image.jpg")),
       images:
        //"MQtLWbD,N8a9CkZ,adCmISK,AedZQ4N,y9qRJR3,brzKTYZ,NSyk07l,EaZiWfn,I1KZdnl,DoQBdzT,slIt2Ww,DA12puU,IYLdRFW,oqmO4Po,T6NaLyI,6XAPrAY,thYzbif,4qmqo3o,8xT2J96,ZCa2pWq,loQfDN2,oabfA68,uOXqDRY,MyyS4vK,fhNYTX4"
         "wxqlQkh,G2Whuq3,0bUSEBX,giP58XN,iKdXwVm,IvpoR40,zJIxPEo,CKlmtPs,fnMylHI,vGXYiYy,MnOB9Le,YqsZKgc,0BJobQo,Otbz312"
           .split(",")
           .map(id => `http://imgur.com/${id}.jpg`)
     };
-
-    StatusBarIOS.setHidden(true);
   }
 
   componentDidMount () {
@@ -51,20 +50,20 @@ class AdvancedEffects extends React.Component {
 
     return (
       <View style={styles.root}>
-
+{/*
         <Banner
           time={time}
           width={viewportW}
           height={viewportH - introH - imgH - transitionH}
         />
-
+*/}
         <Intro
           time={time}
           fps={frames/time}
           width={viewportW}
           height={introH}
         />
-
+{/*
         <Vignette
           time={time}
           width={imgW}
@@ -80,6 +79,7 @@ class AdvancedEffects extends React.Component {
           pauseDuration={0.5}
           transitionDuration={1.5}
         />
+*/}
 
       </View>
     );
---
+++ Examples/Tests/src/index.js
@@ -0,0 +1,220 @@
+const React = require("react-native");
+const {
+  AppRegistry,
+  Text,
+  View,
+  ScrollView,
+  Image,
+} = React;
+
+const Blur = require("./Blur");
+const Add = require("./Add");
+const Multiply = require("./Multiply");
+const Layer = require("./Layer");
+const NativeLayer = require("./NativeLayer");
+const HelloGL = require("./HelloGL");
+const Display2 = require("./Display2");
+const Copy = require("./Copy");
+const TransparentNonPremultiplied = require("./TransparentNonPremultiplied");
+const { width: viewportW, height: viewportH } = require("Dimensions").get("window");
+
+class Tests extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.onLoad = this.onLoad.bind(this);
+    this.onProgress = this.onProgress.bind(this);
+  }
+  onLoad () {
+    console.log("LOADED");
+  }
+  onProgress ({nativeEvent: { progress, loaded, total }}) {
+    console.log("PROGRESS", progress, loaded, total);
+  }
+  render () {
+
+    const debugSize = viewportW / 2;
+
+    const helloGL =
+      <HelloGL width={64} height={64} />;
+
+    const txt =
+      <View style={{ width: 400, height: 400, position: "relative", backgroundColor: "transparent" }}>
+        {[0,1,2,3].map(i => <Text style={{
+          position: "absolute",
+          top: 20+100*i,
+          left: 0,
+          width: 400,
+          height: 100,
+          textAlign: "center",
+          color: ["#f00", "#0f0", "#00f", "#fff"][i],
+          fontSize: 40
+        }}>
+          Hello World {i}
+        </Text>)}
+      </View>;
+
+    const img = "http://i.imgur.com/zJIxPEo.jpg?t="+Date.now();
+
+    const blurredImage =
+      <Blur factor={4} passes={6} width={200} height={200}>
+        {img}
+      </Blur>;
+
+    const blurredImageOverText =
+      <Layer>
+        {blurredImage}
+        {txt}
+      </Layer>;
+
+    return <ScrollView style={{ backgroundColor: "#000" }}>
+      <Display2 width={viewportW} height={viewportW} preload onLoad={this.onLoad} onProgress={this.onProgress}>
+        <Add width={viewportW/2} height={viewportH/2}>
+          {txt}
+          {helloGL}
+        </Add>
+        <Display2 width={viewportW/2} height={viewportH/2} vertical>
+          <Blur factor={1} passes={4} width={viewportW/2} height={viewportH/4}>
+            <Multiply>
+              {blurredImageOverText}
+              {helloGL}
+            </Multiply>
+          </Blur>
+          {blurredImage}
+        </Display2>
+      </Display2>
+
+
+      <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+
+        <NativeLayer width={debugSize} height={debugSize}>
+          <Image source={{ uri: "http://i.imgur.com/S22HNaU.png" }} width={debugSize} height={debugSize} />
+          <Image source={{ uri: "http://i.imgur.com/mp79p5T.png" }} width={debugSize} height={debugSize} />
+        </NativeLayer>
+
+        <NativeLayer width={debugSize} height={debugSize}>
+          <Image source={{ uri: "http://i.imgur.com/S22HNaU.png" }} width={debugSize} height={debugSize} />
+          <Copy width={debugSize} height={debugSize} opaque={false} last>
+            http://i.imgur.com/mp79p5T.png
+          </Copy>
+        </NativeLayer>
+
+        <NativeLayer width={debugSize} height={debugSize}>
+          <Image source={{ uri: "http://i.imgur.com/S22HNaU.png" }} width={debugSize} height={debugSize} />
+          <Copy width={debugSize} height={debugSize} opaque={false} last>
+            <Copy>
+              http://i.imgur.com/mp79p5T.png
+            </Copy>
+          </Copy>
+        </NativeLayer>
+
+        <NativeLayer width={debugSize} height={debugSize}>
+          <Image source={{ uri: "http://i.imgur.com/S22HNaU.png" }} width={debugSize} height={debugSize} />
+          <Copy width={debugSize} height={debugSize} opaque={false} last>
+            <Copy>
+              <Copy>
+                http://i.imgur.com/mp79p5T.png
+              </Copy>
+            </Copy>
+          </Copy>
+        </NativeLayer>
+
+        <NativeLayer width={debugSize} height={debugSize}>
+          <Image source={{ uri: "http://i.imgur.com/S22HNaU.png" }} width={debugSize} height={debugSize} />
+          <Copy width={debugSize} height={debugSize} opaque={false} last>
+            <Copy>
+              <Copy>
+                <Copy>
+                  http://i.imgur.com/mp79p5T.png
+                </Copy>
+              </Copy>
+            </Copy>
+          </Copy>
+        </NativeLayer>
+
+        <NativeLayer width={debugSize} height={debugSize}>
+          <Image source={{ uri: "http://i.imgur.com/S22HNaU.png" }} width={debugSize} height={debugSize} />
+          <Copy width={debugSize} height={debugSize} opaque={false}>
+            <Copy last>
+              <Copy>
+                <Copy>
+                  <Copy>
+                    http://i.imgur.com/mp79p5T.png
+                  </Copy>
+                </Copy>
+              </Copy>
+            </Copy>
+          </Copy>
+        </NativeLayer>
+
+        <NativeLayer width={debugSize} height={debugSize}>
+          <Image source={{ uri: "http://i.imgur.com/S22HNaU.png" }} width={debugSize} height={debugSize} />
+          <NativeLayer>
+            <Image source={{ uri: "http://i.imgur.com/mp79p5T.png" }} width={debugSize} height={debugSize} />
+            <TransparentNonPremultiplied width={debugSize} height={debugSize} premultipliedAlpha>
+              <HelloGL />
+            </TransparentNonPremultiplied>
+          </NativeLayer>
+        </NativeLayer>
+
+        <NativeLayer width={debugSize} height={debugSize}>
+          <Image source={{ uri: "http://i.imgur.com/S22HNaU.png" }} width={debugSize} height={debugSize} />
+          <NativeLayer>
+            <Image source={{ uri: "http://i.imgur.com/mp79p5T.png" }} width={debugSize} height={debugSize} />
+            <TransparentNonPremultiplied width={debugSize} height={debugSize} premultipliedAlpha>
+              <TransparentNonPremultiplied>
+                <HelloGL />
+              </TransparentNonPremultiplied>
+            </TransparentNonPremultiplied>
+          </NativeLayer>
+        </NativeLayer>
+
+        <NativeLayer width={debugSize} height={debugSize}>
+          <Image source={{ uri: "http://i.imgur.com/S22HNaU.png" }} width={debugSize} height={debugSize} />
+          <NativeLayer>
+            <Image source={{ uri: "http://i.imgur.com/mp79p5T.png" }} width={debugSize} height={debugSize} />
+            <TransparentNonPremultiplied width={debugSize} height={debugSize} premultipliedAlpha>
+              <Copy>
+                <TransparentNonPremultiplied>
+                  <Copy>
+                    http://i.imgur.com/S22HNaU.png
+                  </Copy>
+                </TransparentNonPremultiplied>
+              </Copy>
+            </TransparentNonPremultiplied>
+          </NativeLayer>
+        </NativeLayer>
+
+        <NativeLayer width={debugSize} height={debugSize}>
+          <Image source={{ uri: "http://i.imgur.com/S22HNaU.png" }} width={debugSize} height={debugSize} />
+          <Layer width={debugSize} height={debugSize} opaque={false} premultipliedAlpha debug>
+            http://i.imgur.com/mp79p5T.png
+            <TransparentNonPremultiplied>
+              <HelloGL />
+            </TransparentNonPremultiplied>
+          </Layer>
+        </NativeLayer>
+
+        <NativeLayer width={debugSize} height={debugSize}>
+          <Image source={{ uri: "http://i.imgur.com/S22HNaU.png" }} width={debugSize} height={debugSize} />
+          <Layer width={debugSize} height={debugSize} opaque={false}>
+            http://i.imgur.com/mp79p5T.png
+            <TransparentNonPremultiplied>
+              <Copy>
+                <TransparentNonPremultiplied>
+                  <Copy>
+                    http://i.imgur.com/S22HNaU.png
+                  </Copy>
+                </TransparentNonPremultiplied>
+              </Copy>
+            </TransparentNonPremultiplied>
+          </Layer>
+        </NativeLayer>
+
+      </View>
+
+    </ScrollView>;
+  }
+}
+
+module.exports = Tests;
