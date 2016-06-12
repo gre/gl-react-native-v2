a78e1c71ab08ff8206b8a6d3b8a89f853f40521d
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2016-06-12 17:42:47 +0200

    simplify code

--- example/src/Animated/index.js
+++ example/src/Animated/index.js
@@ -1,37 +1,29 @@
 import React, {Component} from "react";
-import {View, Animated } from "react-native";
+import {View, Animated} from "react-native";
 import {AnimatedSurface} from "gl-react-native";
 import GL from "gl-react";
-import Dimensions from "Dimensions";
-const { width: viewportW, height: viewportH } = Dimensions.get("window");
 
-export default class Tests extends Component {
+export default class AnimatedExample extends Component {
   state = {
-    heightValue: new Animated.Value(200)
+    heightValue: new Animated.Value(200),
   };
   componentWillMount () {
     let i = 0;
-    this.interval = setInterval(() => {
+    this.interval = setInterval(() =>
       Animated.spring(this.state.heightValue, {
         toValue: ++i % 2 ? 500 : 200,
-      }).start();
-    }, 1000);
+      }).start(), 1000);
   }
   componentWillUnmount () {
     clearInterval(this.interval);
   }
   render () {
     const { heightValue } = this.state;
-    return <View style={{
-        backgroundColor: "#000",
-        flex: 1,
-        paddingTop: 60,
-        alignItems: "center"
-      }}>
+    return <View style={{ paddingTop: 60, alignItems: "center" }}>
       <AnimatedSurface
         width={200}
         height={heightValue}>
-        <GL.Node shader={{
+        <GL.Node shader={{// inline example
             frag: `
 precision highp float;
 varying vec2 uv;
